737a05fd3fa9656f473ee50d65d8fb26ceaaa9b3
Rifah Zakiah <155142179+Rifah-Zakiah@users.noreply.github.com>
2025-01-08 21:01:12 +0600

    Update shelter.php

--- shelter.php
+++ shelter.php
@@ -16,8 +16,8 @@
     <a href="view_seeks.php">See victims in shelter</a><br>
     <h1>Insert Shelter Details</h1>
         <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
-            <label for="id">Shelter ID:</label><br>
-            <input type="number" name="id" required><br><br>
+            <!-- <label for="id">Shelter ID:</label><br>
+            <input type="number" name="id" required><br><br> -->
             
             <label for="name">Shelter Name:</label><br>
             <input type="text" name="name" required><br><br>
@@ -36,18 +36,18 @@
 
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_SPECIAL_CHARS);
+        //$id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_SPECIAL_CHARS); empty($id)||
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
         $location = filter_input(INPUT_POST, "location", FILTER_SANITIZE_SPECIAL_CHARS);
         $status = filter_input(INPUT_POST, "status", FILTER_SANITIZE_SPECIAL_CHARS);
     }
 
         
-    if (empty($id)||empty($name)||empty($location)||empty($status)) {
+    if (empty($name)||empty($location)||empty($status)) {
         echo"Please enter data";
     }else {
-        $sql = "INSERT INTO Shelter (id, name, location, status)
-                VALUES ('$id', '$name', '$location', '$status')";
+        $sql = "INSERT INTO Shelter (name, location, status)
+                VALUES ('$name', '$location', '$status')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully.";

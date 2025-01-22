a5c44c79a9bec662341fe8695c99bcd8c77ef1f3
Rifah Zakiah <155142179+Rifah-Zakiah@users.noreply.github.com>
2025-01-22 23:10:59 +0600

    Update view_impacts.php

--- view_impacts.php
+++ view_impacts.php
@@ -1,13 +1,25 @@
 <?php
     include("db_dmrs.php");
-    $sql = "SELECT * FROM Impacts";
+
+    // Modified SQL query to join Disaster and Victim tables
+    $sql = "SELECT 
+                i.disaster_id, 
+                d.name AS disaster_name, 
+                i.victim_id, 
+                v.name AS victim_name, 
+                v.phone_no, 
+                v.current_location 
+            FROM Impacts i
+            JOIN Disaster d ON i.disaster_id = d.id
+            JOIN Victim v ON i.victim_id = v.id";
+
     $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <a href="home.php">Go back to home</a><br>
     <title>Impacts Table</title>
+    <link rel="stylesheet" href="style.css">
     <style>
         table {
             width: 100%;
@@ -26,26 +38,38 @@
     </style>
 </head>
 <body>
-    <h1>View Impacts Table</h1>
+    <a href="impacts.php">Add Disaster impacting</a><br>
+    
+    <h1>Disaster Impacting Victims</h1>
     <table>
         <tr>
             <th>Disaster ID</th>
+            <th>Disaster Name</th>
             <th>Victim ID</th>
+            <th>Victim Name</th>
+            <th>Phone Number</th>
+            <th>Current Location</th>
         </tr>
         <?php
         if ($result && $result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>
                         <td>{$row['disaster_id']}</td>
+                        <td>{$row['disaster_name']}</td>
                         <td>{$row['victim_id']}</td>
+                        <td>{$row['victim_name']}</td>
+                        <td>{$row['phone_no']}</td>
+                        <td>{$row['current_location']}</td>
                       </tr>";
             }
         } else {
-            echo "<tr><td colspan='2'>No records found</td></tr>";
+            echo "<tr><td colspan='6'>No records found</td></tr>";
         }
         ?>
     </table><br>
-    <a href="impacts.php">Go Back</a><br>
+    <a href="home.php">Go back to home</a><br>
+    <a href="help.php">Go to get help</a>
+    <a href="helping.php">Go to help</a>
 </body>
 </html>
 <?php

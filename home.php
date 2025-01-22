6fd80ebeecc97910780c8c932bb0bda5be4fe89b
Rifah Zakiah <155142179+Rifah-Zakiah@users.noreply.github.com>
2025-01-22 23:01:26 +0600

    Update home.php

--- home.php
+++ home.php
@@ -4,17 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="homestyle.css">
 </head>
 <body>
     <h1>Welcome to Disaster Management & Relief System Webpage</h1>
-    <br><hr>
-    <a href="victim.php">Need assistance?</a><br>
-    <a href="volunteer.php">Volunteer Registration</a><br>
-    <a href="disaster.php">Add info on disaster</a><br>
-    <a href="shelter.php">Add info on shelter</a><br>
-    <a href="relief.php">Add info on Relief</a><br>
-    <a href="victim_profile.php">Victim profile</a><br>
-    <a href="volunteer_profile.php">Volunteer profile</a><br>
+    <br>
+    <hr>
+        <div class="search-section">
+            <a href="search_help.php" class="btn-primary">🔍 Search for Help</a>
+        </div>
+        <div class="assistance-section">
+            <a href="help.php" class="btn-alert">🚨 Need Assistance?</a>
+            <a href="helping.php" class="btn-success">🤝 Want to Help?</a>
+        </div>
+    <nav>
+        <a href="view_impacts.php" class="links">See disaster impacting victims</a><br>
+        <a href="view_involves.php" class="links">See involvement</a><br><br>
 
+        
+        <!-- <a href="help.php" class="links">Need Assistance?</a><br>
+        <a href="helping.php" class="links">Want to help?</a><br><br> -->
+        <a href="disaster.php" class="links">Add Info on Disaster</a><br>
+        <a href="shelter.php" class="links">Add Info on Shelter</a><br>
+        <a href="relief.php" class="links">Add Info on Relief</a><br><br>
+        <!-- <a href="search_help.php" class="links">Search for help</a><br> -->
+
+        <a href="victim_profile.php" class="links">Victim Profile</a><br>
+        <a href="volunteer_profile.php" class="links">Volunteer Profile</a><br>
+    </nav>
+    <footer>
+        © 2024 Disaster Management & Relief System. All Rights Reserved.
+    </footer>
 </body>
 </html>

3670c0d3a042fd78efe7857efa67e77ae847499c
Rifah Zakiah <155142179+Rifah-Zakiah@users.noreply.github.com>
2025-01-22 23:12:09 +0600

    Update view_seeks.php

--- view_seeks.php
+++ view_seeks.php
@@ -1,12 +1,27 @@
 <?php
     include("db_dmrs.php");
-    $sql = "SELECT * FROM Seeks";
+    
+    // Updated SQL query with JOINs to get victim and shelter details
+    $sql = "SELECT 
+                Seeks.victim_id, 
+                Victim.name AS victim_name, 
+                Victim.phone_no AS victim_phone,
+                Seeks.shelter_id, 
+                Shelter.name AS shelter_name, 
+                Shelter.location AS shelter_location, 
+                Seeks.checkin_date, 
+                Seeks.checkout_date 
+            FROM Seeks
+            JOIN Victim ON Seeks.victim_id = Victim.id
+            JOIN Shelter ON Seeks.shelter_id = Shelter.id";
+
     $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Table</title>
+    <title>Seeks Table</title>
+    <link rel="stylesheet" href="style.css">
     <style>
         table {
             width: 100%;
@@ -26,27 +41,39 @@
 </head>
 <body>
     <a href="home.php">Go back to home</a><br>
-    <h1>Table</h1>
+    <h1>Seeks Table</h1>
     <table>
         <tr>
             <th>Victim ID</th>
+            <th>Victim Name</th>
+            <th>Victim Phone</th>
             <th>Shelter ID</th>
+            <th>Shelter Name</th>
+            <th>Shelter Location</th>
+            <th>Check-in Date</th>
+            <th>Check-out Date</th>
         </tr>
         <?php
         if ($result && $result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>
                         <td>{$row['victim_id']}</td>
+                        <td>{$row['victim_name']}</td>
+                        <td>{$row['victim_phone']}</td>
                         <td>{$row['shelter_id']}</td>
+                        <td>{$row['shelter_name']}</td>
+                        <td>{$row['shelter_location']}</td>
+                        <td>{$row['checkin_date']}</td>
+                        <td>{$row['checkout_date']}</td>
                       </tr>";
             }
         } else {
-            echo "<tr><td colspan='2'>No records found</td></tr>";
+            echo "<tr><td colspan='8'>No records found</td></tr>";
         }
         ?>
     </table><br>
-    <a href="seeks.php">Go Back to seek Shelter</a><br>
-    <a href="shelter.php">Go Back to add Shelter Details</a>
+    <a href="seeks.php">Go Back to Seek Shelter</a><br>
+    <a href="shelter.php">Go Back to Add Shelter Details</a>
 </body>
 </html>
 <?php

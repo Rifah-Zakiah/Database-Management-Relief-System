f8d962b4971f8895a4e1720cc3d18567392755a8
Rifah Zakiah <155142179+Rifah-Zakiah@users.noreply.github.com>
2025-01-22 23:11:38 +0600

    Update view_involves.php

--- view_involves.php
+++ view_involves.php
@@ -1,12 +1,29 @@
 <?php
     include("db_dmrs.php");
-    $sql = "SELECT * FROM Involves";
+
+    // Modified SQL query to join Volunteer, Relief, and Victim tables
+    $sql = "SELECT 
+                i.volunteer_id, 
+                v.name AS volunteer_name, 
+                v.phone_no AS volunteer_phone,
+                i.relief_id, 
+                r.type,
+                i.victim_id, 
+                vt.name AS victim_name, 
+                vt.phone_no AS victim_phone, 
+                vt.current_location
+            FROM Involves i
+            JOIN Volunteer v ON i.volunteer_id = v.id
+            JOIN Relief r ON i.relief_id = r.id
+            JOIN Victim vt ON i.victim_id = vt.id";
+
     $result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Involves Table</title>
+    <link rel="stylesheet" href="style.css">
     <style>
         table {
             width: 100%;
@@ -25,29 +42,43 @@
     </style>
 </head>
 <body>
-    <a href="home.php">Go back to home</a><br>
+    <a href="involves.php">Add involvement in disaster</a><br>
     <h1>Overall Involvement</h1>
     <table>
         <tr>
             <th>Volunteer ID</th>
+            <th>Volunteer Name</th>
+            <th>Volunteer Phone</th>
             <th>Relief ID</th>
+            <th>Relief Type</th>
             <th>Victim ID</th>
+            <th>Victim Name</th>
+            <th>Victim Phone</th>
+            <th>Victim Location</th>
         </tr>
         <?php
         if ($result && $result->num_rows > 0) {
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>
                         <td>{$row['volunteer_id']}</td>
+                        <td>{$row['volunteer_name']}</td>
+                        <td>{$row['volunteer_phone']}</td>
                         <td>{$row['relief_id']}</td>
+                        <td>{$row['type']}</td>
                         <td>{$row['victim_id']}</td>
+                        <td>{$row['victim_name']}</td>
+                        <td>{$row['victim_phone']}</td>
+                        <td>{$row['current_location']}</td>
                       </tr>";
             }
         } else {
-            echo "<tr><td colspan='3'>No records found</td></tr>";
+            echo "<tr><td colspan='9'>No records found</td></tr>";
         }
         ?>
     </table><br>
-    <a href="involves.php">Go Back</a>
+    <a href="home.php">Go Back to Home</a>
+    <a href="help.php">Go to get help</a>
+    <a href="helping.php">Go to help</a>
 </body>
 </html>
 <?php

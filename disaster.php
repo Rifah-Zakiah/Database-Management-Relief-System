c6e3bcea52e1eac874494f21813b8927fc37a215
Rifah Zakiah <155142179+Rifah-Zakiah@users.noreply.github.com>
2025-01-22 23:00:59 +0600

    Update disaster.php

--- disaster.php
+++ disaster.php
@@ -9,11 +9,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Disaster info</title>
+    <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
-    <a href="home.php">Go back to home</a><br>
-    <a href="search_disaster.php"> Search in Disaster Table </a><br>
-    <a href="impacts.php">See disaster impacting victims</a><br>
+    <a href="search_disaster.php"> Search Disaster Info </a>
     <h1>Insert Disaster Details</h1>
         <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
             <!-- <label for="id">Disaster ID:</label><br>
@@ -27,26 +26,35 @@
             
             <label for="severity">Severity (1-10):</label><br>
             <input type="number" name="severity" min="1" max="10" required><br><br>
+
+            <label for="start_date">Start date:</label><br>
+            <input type="date" name="start_date"><br><br>
+
+            <label for="end_date">End date:</label><br>
+            <input type="date" name="end_date"><br><br>
             
             <button type="submit">Submit</button>
         </form>
-        <a href="show_disaster.php">Disaster Table</a><br>
+        <a href="home.php">Go back to home</a>
+        <a href="show_disaster.php">Disaster Info</a>
 </body>
 </html>
 
 <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-       // $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_SPECIAL_CHARS);empty($id)||
+        //$id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_SPECIAL_CHARS);empty($id)||
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
         $region = filter_input(INPUT_POST, "region", FILTER_SANITIZE_SPECIAL_CHARS);
         $severity = filter_input(INPUT_POST, "severity", FILTER_SANITIZE_SPECIAL_CHARS);
+        $start_date = filter_input(INPUT_POST, "start_date", FILTER_SANITIZE_SPECIAL_CHARS);
+        $end_date = filter_input(INPUT_POST, "end_date", FILTER_SANITIZE_SPECIAL_CHARS);
     }
 
     if (empty($name)||empty($region)||empty($severity)) {
         echo"Please enter data";
     }else {
-        $sql = "INSERT INTO Disaster (name, region, severity)
-            VALUES ('$name', '$region', '$severity')";
+        $sql = "INSERT INTO Disaster (name, region, severity, start_date, end_date)
+            VALUES ('$name', '$region', '$severity', '$start_date', '$end_date')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully.";
